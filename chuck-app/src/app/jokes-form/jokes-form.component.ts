e34e37592cbe61f8396e3d7a0d1466a8fe54070b
Bujenita <claudiu.bujenita@centric.eu>
2023-03-28 10:55:24 +0300

    custom validator

--- chuck-app/src/app/jokes-form/jokes-form.component.ts
+++ chuck-app/src/app/jokes-form/jokes-form.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
+  AbstractControl,
   FormControl,
   FormGroup,
   FormsModule,
   ReactiveFormsModule,
+  ValidationErrors,
   Validators,
 } from '@angular/forms';
 
@@ -20,7 +22,23 @@ export class JokesFormComponent {
   author: string = '';
   newJokeForm: FormGroup = new FormGroup({
     joke: new FormControl('', [Validators.required, Validators.minLength(4)]),
-    author: new FormControl('', Validators.required),
+    author: new FormControl('', [
+      Validators.required,
+      this.customValidator('s'),
+    ]),
   });
   onSubmit(newJokeForm: any) {}
+
+  public customValidator(startsWithLetter: string) {
+    return (control: AbstractControl) => {
+      console.log(control.value);
+      if (control.value[0] === startsWithLetter) {
+        return null;
+      } else {
+        return {
+          startsWithLetterValidation: `the input doesn't start with ${startsWithLetter}`,
+        };
+      }
+    };
+  }
 }
